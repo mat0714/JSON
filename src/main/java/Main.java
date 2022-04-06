3f7dc59d085007f31b513c24c315ee15d7268aee
Mateusz Kolodziejczyk <96115456+mat0714@users.noreply.github.com>
2022-04-06 11:23:29 +0200

    Added "something".

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("JSON change");
+        System.out.println("something");
 
         String line = null;
         FileReader fileReader;
