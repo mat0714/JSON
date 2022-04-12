992ea396f1e647be9e05fc0e4b5c52e4060adf26
Mateusz Kolodziejczyk <96115456+mat0714@users.noreply.github.com>
2022-04-12 10:26:01 +0200

    Edit on Github2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Git: Hello");
+        System.out.println("Git: Hello2");
 
         String line = null;
         FileReader fileReader;
